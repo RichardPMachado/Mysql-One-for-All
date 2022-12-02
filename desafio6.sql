f683f4b2014f07162b74708dd5399144276fbee1
RichardPMachado <richardmachado2211@gmail.com>
2022-12-02 20:35:10 -0300

    feat[requisito 6]: completo

--- desafio6.sql
+++ desafio6.sql
@@ -1,8 +1,8 @@
 SELECT
-	MIN(p.preco) AS faturamento_minimo,
-    MAX(p.preco) AS faturamento_maximo,
-    ROUND(AVG(p.preco), 2) AS faturamento_medio,
-    SUM(p.preco) AS faturamento_total
-FROM SpotifyClone.planos AS p
-LEFT JOIN SpotifyClone.usuarios AS u
+    FORMAT(MIN(p.preco), 2) AS faturamento_minimo,
+    FORMAT(MAX(p.preco), 2) AS faturamento_maximo,
+    FORMAT(AVG(p.preco), 2) AS faturamento_medio,
+    FORMAT(SUM(p.preco), 2) AS faturamento_total
+FROM SpotifyClone.planos AS p 
+INNER JOIN SpotifyClone.usuarios AS u
 ON p.plano_id = u.plano_id;
